79191633f429d23f556c5d5ab09eda074b9a8129
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2018-09-09 21:10:55 -0300

    update

---
+++ src/main/java/com/friendtrip/api/services/ExemploService.java
@@ -0,0 +1,11 @@
+package com.friendtrip.api.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExemploService {
+
+	public void testarService() {
+		System.out.println("### Executando serviço de teste!!!");
+	}
+}
